0f03152bd49cf8bae0a7130936e5d7499ce1ff3e
Ernest <ernest20145@gmail.com>
2023-01-30 05:36:52 +0200

    added avatar,firstname lastname , email , phone , publication subscription,description for schema

--- Schema/UserSchema.js
+++ Schema/UserSchema.js
@@ -6,9 +6,38 @@ const UserSchema = new Schema({
         require: true,
         unique: true
     },
+    avatar:{
+        type:String,
+    },
+    firstname:{
+        type:String,
+        require: true
+    },
+    lastname:{
+        type:String,
+        require:true
+    },
     password:{
         type:String,
-        require: true,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true,
+        unique: true
+    },
+    phone:{
+        type:String,
+        require:true,
+    },
+    publication:{
+        type:Array,
+    },
+    subscribtion:{
+        type:Array,
+    },
+    description:{
+        type:String,
     }
 }, {
     timestamps:true
