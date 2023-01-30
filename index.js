eb505ab973ad10f1595f7e9bb5912e049652c5f4
Demetrovichz <pugehok322@gmail.com>
2023-01-30 05:27:59 +0300

    add UserSchema

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ const PORT=4444;
 
 
 const app = express();
+
+mongoose
+.connect('mongodb+srv://pugehok:3429382q@cluster0.klurzlj.mongodb.net/?retryWrites=true&w=majority')
+.then(()=> console.log("База данных запущена"))
+.catch((e)=> console.log(`База данных не смогла запуститься ${e}`))
 app.use(express.json())
 
 
 app.listen(PORT, (req,res)=>{
     console.log(`Server succeful started on port ${PORT}`)
-})
+});
 
 app.get('/cards', (req,res) =>{
     
-})
+});
 
 
-app.post('/')
+app.post('/', (req,res) =>{
+
+});
